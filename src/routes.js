15e82758a9c48e11988cf9c691c057166021b8f3
Lucas <g.lucas.silva@gmail.com>
2020-05-27 19:34:32 -0400

    Update Login, Profile and Incidents
    Update Login, Profile and Incidents

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentsController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController')
+
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
@@ -10,5 +13,9 @@ routes.post('/ongs', OngController.create);
 routes.post('/incidents', IncidentsController.create);
 routes.get('/incidents', IncidentsController.index);
 routes.delete('/incidents/:id',IncidentsController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/sessions', SessionController.create);
 
 module.exports = routes;
