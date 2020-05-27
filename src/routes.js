085ac61ef72549f50a9a9e93bc1274afd18fbf45
Lucas <lucas.goncalves@flexpeak.com.br>
2020-05-27 02:38:33 -0400

    Listar, Store e Delete ONG
    Listar, Store e Delete ONG

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentController');
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
-    return response.json({
-        evento : 'Festa',
-        aluno : 'Lucas'
-    })
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.post('/incidents', IncidentsController.create);
+routes.get('/incidents', IncidentsController.index);
+routes.delete('/incidents/:id',IncidentsController.delete);
 
 module.exports = routes;
